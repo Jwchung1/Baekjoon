09cb8b17350d94ede1ace038ccc7302fe7e02445
Jwchung1 <jiwoo@DESKTOP-54KAS4K>
2024-07-30 15:20:56 +0900

    20240730

---
+++ Platinum/1761.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> tree[40000]
+int main()
+{
+    cin >> N;
+    for(int i=0; i<N; i++)
+    {
+
+    }
+    cin >> M;
+}
